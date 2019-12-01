2cbffd8825924b05d7aa9cb97012ff11ca7e5be3
Adam DeLessio <adamdelessio@gmail.com>
2019-12-01 14:30:24 -0500

    Did a lot because I forgot

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// EXIT BUTTON
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', close)
+
+function close() {
+	document.querySelector('section').style.display = 'none'
+}
+
